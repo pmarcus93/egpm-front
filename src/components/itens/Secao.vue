b809a73d7057a09c7380e6879a9e17627532be16
Marcus Pereira <pereiramarcus93@gmail.com>
2019-04-26 20:11:46 -0300

    Ajustando Secao e Banner

--- src/components/itens/Secao.vue
+++ src/components/itens/Secao.vue
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <section class="my-4">
-                    <h1 class="text-center">{{titulo}}</h1>
+                <section>
+                    <h1 class="text-center my-4">{{titulo}}</h1>
                     <p>{{descricao}}</p>
                 </section>
             </div>
