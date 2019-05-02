4f3c512854a296e309210ca84cb6848049ce7afe
Lucas Junior Dias de Paula <lucas.dias@unyleya.com.br>
2019-05-02 11:44:26 -0300

    Implementação de Parallax em secção

--- src/components/itens/SecaoLeft.vue
+++ src/components/itens/SecaoLeft.vue
@@ -1,31 +1,23 @@
 <template>
-
-        <div class="secao-left-body">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-12 py-15">
-
-                        <div class="col-sm-6 my-auto secao-left">
-                            <section>
-                                <h1 class="text-left mb-4 titulo-secao-left"  v-bind:style="{ 'border-bottom':  colorborder + ' solid 3px' }" >{{titulo}}</h1>
-                                <p class="mt-3">{{descricao}}</p>
-                            </section>
-                        </div>
-
-                        <div class="fundo-secao"
-                             v-bind:style="{ 'background': 'url(' + imagem + ') center/cover' }"></div>
-
+    <div class="secao-left-body">
+        <div class="container-fluid">
+            <div class="row">
+                <section class="section static d-flex" :class="classimage">
+                    <div class="col-sm-6 my-auto box-info">
+                        <h1 class="text-left mb-4 titulo-secao-left"
+                            v-bind:style="{ 'border-bottom':  colorborder + ' solid 3px' }">{{titulo}}</h1>
+                        <p class="mt-3">{{descricao}}</p>
                     </div>
-                </div>
+                </section>
             </div>
         </div>
-
+    </div>
 </template>
 
 <script>
     export default {
         name: "SecaoLeft",
-        props: ["imagem", "descricao", "titulo", "colorborder"],
+        props: ["imagem", "descricao", "titulo", "colorborder", "classimage"],
     }
 </script>
 
@@ -39,21 +31,42 @@
         color: whitesmoke;
     }
 
-    .fundo-secao {
-        height: 100%;
-        width: 100%;
+    .box-info {
+        z-index: 2;
+    }
+
+    .static::after{
+        content: " ";
         position: absolute;
         top: 0;
         right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 0;
+    }
+
+    .section {
+        position: relative;
+        height: 100vh;
+        justify-content: flex-start;
+    }
+
+    .parallax::after {
+        content: " ";
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+
+        transform: translateZ(-1px) scale(1.9);
+        background-size: 100%;
+        z-index: -1;
+    }
+
+    .bgpalestra::after {
+        background: url('https://lh3.googleusercontent.com/3qV4HSPakRghRhQACieKIo10v9VPzy-kQ8bWV_wfAkEPi9Bwia3eB8qudZG4uAeX-UgrbqCOlR_lZps7QCeCraSY2uxnnR7qrz4jkROdNJREZzWTqhcfFWGyW4lkk_YZWIT2zn6jZzwn9Rlcx0N8SC7oOUR6-sWO_A3crFvVui1Smem_YrrmhAiecU7IubGxgUFtUiefbFWIqlNaqYTwcHwHqeqzH3QUiwyL1tUYYWdOK0Vau6u6lrI4k4tghPVC36dAO0RwmHrK4Hmpw7Qi2ySJajD7-pni1nXRGNwI4L9QpT-xfletQXMyA2fAUkc3V0ovCQMEgZvXpMu9Ur1WHC0iZqDyQ5gNR_CuPB0lxcEdtJPnaHnr_InN1agqycCKq4g1MD8Y0rq_cMgMyxD5TFMxXtby4Hhjcaxzkt-G06uq7lG-uKgeSivv_c2Pf9-druUMBc58hfIBfMD-HdCvJie0vZdVA6J6_40rotVrqYIvBXQsHA8tnilabv6a1fQ_rMTFOGEcCD10m0U1fvywO8LhoGafVGLrh-RObXHNA2ae96acQOqI1dwCHhi1sZOXtRkSrc6I0LQLzfs4r2ONOTV5c4kFiDw17iaM0WLe-uDzq40pvuenMGJm40pYqbBFavx3oV_cxClA_fm9mKgzi2w2uZuWcRQ_=w1760-h990-no') center/cover;
 
     }
 
-    .secao-left {
-        z-index: 3;
-    }
-
-    .py-15 {
-        padding-top: 15%;
-        padding-bottom: 15%;
-    }
 </style>
--- src/components/itens/SecaoRight.vue
+++ src/components/itens/SecaoRight.vue
@@ -2,18 +2,13 @@
     <div class="secao-right-body">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-sm-12 py-15">
-
-                    <div class="col-sm-6 my-auto secao-right float-right">
-                        <section>
-                            <h1 class="text-left mb-4 titulo-secao-right" v-bind:style="{ 'border-bottom':  colorborder + ' solid 3px' }">{{titulo}}</h1>
-                            <p class="mt-3">{{descricao}}</p>
-                        </section>
+                <section class="section parallax d-flex" :class="classimage">
+                    <div class="col-sm-6 my-auto ">
+                        <h1 class="text-left mb-4 titulo-secao-right"
+                            v-bind:style="{ 'border-bottom':  colorborder + ' solid 3px' }">{{titulo}}</h1>
+                        <p class="mt-3">{{descricao}}</p>
                     </div>
-
-                    <div class="fundo-secao" v-bind:style="{ 'background': 'url(' + imagem + ') center/cover' }"></div>
-
-                </div>
+                </section>
             </div>
         </div>
     </div>
@@ -22,12 +17,13 @@
 <script>
     export default {
         name: "SecaoRight",
-        props: ["titulo", "imagem", "descricao", "colorborder"],
+        props: ["titulo", "imagem", "descricao", "colorborder", "classimage", "parallax"],
     }
 </script>
 
 <style scoped>
-    .titulo-secao-right{
+
+    .titulo-secao-right {
         font-size: 4em;
         font-family: Archive;
     }
@@ -36,21 +32,31 @@
         color: whitesmoke;
     }
 
-    .fundo-secao {
-        height: 100%;
-        width: 100%;
+    .section {
+        position: relative;
+        height: 100vh;
+        justify-content: flex-end;
+    }
+
+    .parallax::after {
+        content: " ";
         position: absolute;
         top: 0;
         right: 0;
+        bottom: 0;
+        left: 0;
 
+        transform: translateZ(-2px) scale(2.1);
+        background-size: 100%;
+        z-index: -1;
     }
 
-    .secao-right {
-        z-index: 3;
+    .bgcampeonatos::after {
+        background: url('https://lh3.googleusercontent.com/4gMAYZ9SGF6GtFbnKB5DGLU0nD8fTvXdCbTV8PF4xqp6Ua3EjBsqjgNO-5xSzoM_D1a5v57nzO-Lsz764mOmmHenFpgK3-cg6CRjsHUZRq-0mnAhpZigO8ZInsaNW7SnMsQ9eSUy01iRIQ5k4lWkx4Jk83Sq6mDRPLRMTgIHnsxgN3tev91FR8emMhh8mNEIGdT1ogOIpxU7lZ_QYS1_NCkjWQ90QnkpVSPtJkrDFWOLb5pVEN-oC9FiM5j6JOSr6y0Q59JiwQrhKy0ebT5BPLmHAcW2rvznJjKgZAw8HR1q7dIjTNMcKxppOkLWncj9XXf2QAvOLzJlb11zNJ_4RBNFYD60jAiz5fLLb6Joh1ofTP2C67vSbOz0avwipZg_IQiJ8L-89_6qP30kLfsiBgkvMbeyTmM2jlJkcTCfLwcPnXQ-lsop80lIL9PWKq9awOo8pS2Fcdj84DPZUYCJbOtb6ahEsLtuSFmtEPz5gLVlzDJfuS8eYwtSR0XcDVm_NRqo1zkHh8047bmC9-uRjx-X5dPzgomNoWSeOUCQArXgdA433zLeZVk5Ny4mQuvR3Nggor27G-LOmNY7VkEK6AtiSi1H_JiHuk9p_ONOjmyl1yNDw6d09gR-_BV8p_3Gj5xHayd4qbUiQf8fanNZkRN1jO-S29sH=w1760-h990-no') center/cover;
     }
 
-    .py-15 {
-        padding-top: 15%;
-        padding-bottom: 15%;
+    .bgfreeplay::after {
+        background: url('https://lh3.googleusercontent.com/ZONfvR89XgEOzdoO0G4bu7f-Z7QB1QuaXL3uA810A_GnEATz99xho_zV1stBwuNRx-NG89ArIPyvCLme5EQcxVcr6WeKSFWVYeg1HD-cpfsmijnCdtXDO43CRDWJvFhhI9TFZwdr0da-i8W32PRhuNHh_5RXoPh1o8gkFyk9__Wf_NFi3M0z-oYBpQ5KKWUnMb8Vt9MhDHPa-GmbHbmUuJxAWxW4WxqIPPI8isWfAN6XNI0Qvhr2cV64Z71HsFsFqU2d-AQVKYLqYRpAKC5MGHwVSQBO3_B9-mx5qH1bItIGtW0l69GY2hcYg868tXF7jwaWbb6BeuOjHwBIzPgycBubFPYXlVJUcsQSV8NEtvv4LjYQFCVwLZYMkfsHMUb8n7OMPSJQ3QXm4bYQcrCbqVWgKY2tl9z7aoc_nmzYtd4NFnixbLENn1obZ3IFfSOitMdglSuF9Gho9IS6GPSD0Ed_6MN-tV0bJaygP4xP_0MwIzju3j2F2w23ZwOjVziK7ld6CIX6flBqRO_qAHBbQkTwQxynIy_TiFhI-R0sjWstDmuDJWdV_18orA3Gf_EFvLmVaWjSDRtnbxAhmU7CceJdMnsOyWR_VbWe-lrEnlrU0YryqWB-V0NWVlG0J6AjnNFZCAzN_N5oDPTK7vphrgktFH909MDp=w1762-h990-no') center/cover;
     }
+
 </style>
