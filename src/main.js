b81ff48ed1c61da14869d59b1f1442e95747094b
Marcus Pereira <marcus.pereira@unyleya.com.br>
2019-05-29 09:05:20 -0300

    Removendo parametros de assinatura de método nunca utilizados.

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ Vue.use(VueRouter)
 const router = new VueRouter({
     mode: 'history',
     routes,
-    scrollBehavior(to, from, savedPosition) {
+    scrollBehavior() {
         return {x: 0, y: 0};
     }
 });
