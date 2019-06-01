420d1f42167a8359ca79c9f544dec018d03293f4
Marcus Pereira <pereiramarcus93@gmail.com>
2019-06-01 10:11:21 -0300

    Midia: ajustando exibição em certos breakpoints.

--- src/components/telas/Midia.vue
+++ src/components/telas/Midia.vue
@@ -2,18 +2,23 @@
     <div class="midia">
         <div class="container">
             <div class="row">
-                <Secao titulo="#EGPM na mídia"
-                       descricao="Saiba mais sobre o EGPM!"></Secao>
-                <div class="col-xl-6 col-sm-12">
+                <div class="col-lg-6 my-auto">
+                    <Secao titulo="#EGPM na mídia"
+                           descricao=""></Secao>
+                </div>
+                <div class="col-lg-6">
+
                     <div class="d-flex justify-content-center">
                         <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/wW9VDhyh2C0"
                                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
                     </div>
+
                 </div>
+
             </div>
 
             <div class="row">
-                <div class="col-12 titulo-depoimentos mt-5 text-center">O QUE ELES ACHARAM DO EGPM</div>
+                <div class="col-12 titulo-depoimentos mt-5 text-center">O QUE ELES ACHARAM DO #EGPM</div>
 
                 <Depoimento
                         depoimento="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indust ry's standard dummy text ever since the 1500s, whent was popularised in the 1960s with the release of Letr aset she"
