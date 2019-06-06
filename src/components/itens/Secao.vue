a5c5b9c393cb076a3a5d5b4f95bbfc21f35292a4
Marcus Pereira <marcus.pereira@unyleya.com.br>
2019-06-06 11:02:45 -0300

    Secao: Organizando CSS

--- src/components/itens/Secao.vue
+++ src/components/itens/Secao.vue
@@ -23,19 +23,12 @@
         src: url("../../assets/fonts/Archive-Regular.ttf");
     }
 
-    /* Small devices (landscape phones, 576px and up) */
-    @media (max-width: 576px) {
-        .titulo-secao {
-            font-family: $font-archive;
-            font-size: 2.5em;
-            text-align: center !important;
-        }
-    }
+    .titulo-secao {
+        font-family: $font-archive;
+        font-size: 2.5em;
+        text-align: center !important;
 
-    /* Medium devices (tablets, 768px and up) */
-    @media (min-width: 576px) {
-        .titulo-secao {
-            font-family: $font-archive;
+        @include sm {
             font-size: 3em;
         }
     }
