1e794aa9e4f5a758098797b73ece9573ac4c2cbd
Lucas <lucasmg37@gmail.com>
2019-06-06 21:45:15 -0300

    Alterando fundo de Descrição

--- src/components/itens/BannerJogo.vue
+++ src/components/itens/BannerJogo.vue
@@ -1,30 +1,64 @@
 <template>
-    <div class="container">
-        <div class="row py-5">
+    <div>
 
+        <div class="row py-5 position-relative">
+            <div class="col-12 banner-foto" :style="'background: url(' + imagem + ') center/cover' "></div>
+            <div class="col-12 sobre-banner"></div>
+            <div class="col-12 no-gutters">
+                <div class="row no-gutters">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6 col-st-12 my-auto info-game">
+                                <h1 class="titulo-secao text-center mb-4">{{titulo}}</h1>
 
-            <div class="col-md-6 col-st-12 my-auto info-game">
-                <h1 class="titulo-secao text-center mb-4">{{titulo}}</h1>
+                                <p class="texto-info" v-if="descricao">
+                                    {{descricao}}
+                                </p>
 
-                <p class="texto-info" v-if="descricao">
-                    {{descricao}}
-                </p>
+                                <div class="my-3">
+                                    <div class="texto-info" v-if="estilo"><b>Categoria: </b>{{estilo}}</div>
+                                    <div class="texto-info" v-if="estilo"><b>Classificação indicativa: </b>18 anos</div>
+                                </div>
 
-                <div class="my-3">
-                    <div class="texto-info" v-if="estilo"><b>Categoria: </b>{{estilo}}</div>
-                    <div class="texto-info" v-if="estilo"><b>Classificação indicativa: </b>18 anos</div>
+                            </div>
+
+                            <div class="col-md-6 col-st-12 align-middle text-center">
+                                <iframe width="100%" height="315" :src="video" frameborder="0"
+                                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                        allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-            </div>
-
-            <div class="col-md-6 col-st-12 align-middle text-center">
-                <iframe width="100%" height="315" :src="video" frameborder="0"
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen></iframe>
             </div>
         </div>
-    </div>
 
+
+        <!--        <div class="container">-->
+        <!--            <div class="row py-5">-->
+
+        <!--                <div class="col-md-6 col-st-12 my-auto info-game">-->
+        <!--                    <h1 class="titulo-secao text-center mb-4">{{titulo}}</h1>-->
+
+        <!--                    <p class="texto-info" v-if="descricao">-->
+        <!--                        {{descricao}}-->
+        <!--                    </p>-->
+
+        <!--                    <div class="my-3">-->
+        <!--                        <div class="texto-info" v-if="estilo"><b>Categoria: </b>{{estilo}}</div>-->
+        <!--                        <div class="texto-info" v-if="estilo"><b>Classificação indicativa: </b>18 anos</div>-->
+        <!--                    </div>-->
+
+        <!--                </div>-->
+
+        <!--                <div class="col-md-6 col-st-12 align-middle text-center">-->
+        <!--                    <iframe width="100%" height="315" :src="video" frameborder="0"-->
+        <!--                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"-->
+        <!--                            allowfullscreen></iframe>-->
+        <!--                </div>-->
+        <!--            </div>-->
+        <!--        </div>-->
+    </div>
 </template>
 
 <script>
@@ -35,7 +69,8 @@
             'lancamento',
             'estilo',
             'video',
-            'descricao'
+            'descricao',
+            'imagem'
         ]
     }
 </script>
@@ -55,6 +90,23 @@
 
     .titulo-secao {
         font-weight: 600;
+    }
+
+    .sobre-banner {
+        width: 100%;
+        height: 100%;
+        top: 0;
+        background: rgba(0, 0, 0, .8);
+        position: absolute;
+        z-index: -1;
+    }
+
+    .banner-foto {
+        top: 0;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        z-index: -2;
     }
 
 </style>
