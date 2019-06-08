8a763370ac82197d07d56ea18ce37139d03a79cb
Marcus Pereira <pereiramarcus93@gmail.com>
2019-06-08 10:35:41 -0300

    BannerMenor: polindo exibição da tela.

--- src/components/itens/BannerMenor.vue
+++ src/components/itens/BannerMenor.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="row">
         <div class="col-12 banner text-center">
-            <h1 class="titulo-secao">{{titulo}}</h1>
-            <p class="texto-secao">{{descricao}}</p>
+            <h1 class="titulo-secao"><span>{{titulo}}</span></h1>
+            <p class="texto-secao mt-3">{{descricao}}</p>
         </div>
     </div>
 </template>
@@ -15,24 +15,41 @@
 </script>
 
 <style scoped lang="scss">
+
     .banner {
-        padding: 70px 0;
+        padding: 50px 0;
     }
 
     .texto-secao {
         color: $color-primary;
-        font-size: 1.5em;
+        @include sm {
+            font-size: 1.3rem;
+        }
     }
 
     .titulo-secao {
         font-family: $font-archive;
-        font-size: 3em;
+        font-size: 2rem;
         color: $color-primary;
 
         @include sm {
-            font-size: 5em;
+            font-size: 3rem;
+        }
+
+        span {
+            position: relative;
+            padding-bottom: 1px;
+        }
+
+        span::after {
+            bottom: 0;
+            border-bottom: 1px solid $color-primary;
+            content: "";
+            height: 1px;
+            left: 0;
+            position: absolute;
+            width: 100%;
         }
     }
-
 
 </style>
