6f11debfe23f8ed6aa5e1a3d1a02ce9125e5c112
Marcus Pereira <pereiramarcus93@gmail.com>
2019-06-08 17:51:10 -0300

    Duvidas: removendo telas não utilizadas + ajustando rotas.

--- src/components/telas/Duvida.vue
+++ src/components/telas/Duvida.vue
@@ -2,41 +2,27 @@
     <div class="container duvida">
         <div class="row no-gutters">
 
-            <div class="col-md-12 col-lg-3 d-flex">
-                <div class="secao-duvidas">
-                    <section>
-                        <h1 class="my-4 titulo-secao">Dúvidas</h1>
-                        <p class="mt-3 subtitulo">As dúvidas mais frequentes, respondidas aqui! <br>
-                            <router-link to='/duvidas'>
-                                <button class="btn btn-light my-1">Outras dúvidas</button>
-                            </router-link>
-                        </p>
-                    </section>
+            <div class="col-12">
+                <h1 class="my-4 titulo-secao text-center">Dúvidas</h1>
+                <p class="mt-3 subtitulo text-center">As dúvidas mais frequentes, respondidas aqui! <br>
+
+                <div class="accordion my-5" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h5 class="mb-0">
+                                <button class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    Menores de idade podem participar do evento?
+                                </button>
+                            </h5>
+                        </div>
+
+                        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                                 anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-
-            <div class="col-md-12 col-lg-3">
-                <ItemDuvida
-                        color="#3BAC7A"
-                        duvida="Menores de idade podem participar do envento?"
-                        resposta="Sim. Desde que acompanhado por um responsável."
-                ></ItemDuvida>
-            </div>
-
-            <div class="col-md-12 col-lg-3">
-                <ItemDuvida
-                        color="#EA2A3A"
-                        duvida="Ingresso é válido para os 2 dias de evento?"
-                        resposta="Sim. O ingresso é único e válido para ambos os dias do evento."
-                ></ItemDuvida>
-            </div>
-
-            <div class="col-md-12 col-lg-3">
-                <ItemDuvida
-                        color="#3255A5"
-                        duvida="Posso participar de mais de um campeonato?"
-                        resposta="Sim. Desde que não haja conflito de horários."
-                ></ItemDuvida>
             </div>
         </div>
     </div>
@@ -52,6 +38,14 @@
 </script>
 
 <style lang="scss" scoped>
+
+    .card-header {
+        background-color: $rainbow-green;
+        font-family: $font-archive;
+        button {
+            color: $light-text;
+        }
+    }
 
     .duvida {
         color: $color-title;
