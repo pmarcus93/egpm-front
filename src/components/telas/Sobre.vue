a7c728b1ee50fc4b5706a986210d67e76196f0d6
Marcus Pereira <pereiramarcus93@gmail.com>
2019-06-09 13:39:36 -0300

    Banner: fazendo ajustes no banner inicial.

--- src/components/telas/Sobre.vue
+++ src/components/telas/Sobre.vue
@@ -5,11 +5,10 @@
                 <div class="col-lg-6 my-auto">
                     <Secao titulo="Sobre o #EGPM3"
                            descricao="Realizado na FAPAM, o Encontro Gamer de Pará de Minas é o maior evento de videogames da cidade.
-           Com campeonatos, palestras e free play, são dois dias de muito aprendizado, competição e diversão!
-           Temos jogo de esporte, musicais, de dança, de luta... traga toda a família e se divirta!
-           Acompanhe a hashtag oficial do evento nas redes sociais e fique por dentro de datas e atrações! #EGMP3">
+                                           Com campeonatos, palestras e free play, são dois dias de muito aprendizado, competição e diversão!
+                                           Temos jogo de esporte, musicais, de dança, de luta... traga toda a família e se divirta!
+                                           Acompanhe a hashtag oficial do evento nas redes sociais e fique por dentro de datas e atrações! #EGMP3">
                     </Secao>
-
                 </div>
                 <div class="col-lg-6">
                     <BlocoIcons>
