0b837fbecb6e08eaf4cd0b24708b4b1ef7c40c56
Marcus Pereira <pereiramarcus93@gmail.com>
2019-06-09 13:37:10 -0300

    Banner: fazendo ajustes no banner inicial.

--- src/components/telas/Banner.vue
+++ src/components/telas/Banner.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="banner">
-        <section>
-            <h2><span class="badge badge-pill badge-em-breve">Em breve!</span></h2>
-            <div class="logo-soon-group">
-                <img class="logo-full img-fluid d-block mx-auto my-5" src="../../assets/full-logo.png"
+    <div class="background-banner">
+        <div class="container-fluid">
+            <section class="py-5 text-center">
+                <h2 class="text-center"><span class="badge badge-pill badge-em-breve">Em breve!</span></h2>
+                <img class="logo-full img-fluid d-block mx-auto my-4" src="../../assets/full-logo.png"
                      alt="Logo do Terceiro Encontro Gamer de Pará de Minas">
-            </div>
-            <div class="descricao text-center"><p>O maior evento de videogames da cidade.<br>Feito por gamers,
-                para gamers.</p></div>
-        </section>
+                <div class="descricao"><p>O maior evento de videogames da cidade.<br>Feito por gamers,
+                    para gamers.</p></div>
+            </section>
+        </div>
     </div>
 </template>
 
@@ -20,17 +20,8 @@
 
 <style lang="scss" scoped>
 
-    .banner {
-        margin-top: 30px;
-        padding: 0 10px;
-        align-items: center;
+    .background-banner {
         background: url("../../assets/banner-background.jpg") center/cover;
-        display: flex;
-        height: 70vh;
-        justify-content: center;
-        @include sm {
-            height: 80vh;
-        }
     }
 
     .badge-em-breve {
@@ -44,32 +35,10 @@
 
     }
 
-    .destaque {
-        background: $color-primary;
-        color: $color-title;
-        font-size: 20px;
-        font-weight: bold;
-        text-align: center;
-        max-width: 125px;
-
-        @include lg {
-            font-size: 30px;
-            max-width: 200px;
-        }
-    }
-
-    .logo-full {
-        width: 350px;
-    }
-
-    .logo-soon-group {
-        margin-top: 100px;
-    }
-
     .descricao {
         color: $color-primary;
         text-transform: uppercase;
-        font-size: 2.5vh;
+        font-size: 2.7vh;
         @include sm {
             font-size: 1.5rem;
         }
