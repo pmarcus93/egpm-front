6cef67f527ff6996284f62a9e74daa059a8b8918
Lucas <lucasmg37@gmail.com>
2019-06-10 13:43:07 -0300

    Corrigindo Erro 404 em capeonatos. Adicionando texto salvo do servidor no span

--- src/components/itens/Links.vue
+++ src/components/itens/Links.vue
@@ -2,10 +2,7 @@
     <div>
         <div class="col-12 py-4 links text-center align-content-center align-middle">
             <router-link to="/inicio">
-                <button class="btn btn-light"> Inicio</button>
-            </router-link>
-            <router-link to="/duvidas">
-                <button class="btn btn-light mx-3"> Dúvidas</button>
+                <button class="btn btn-light mx-3"> Inicio</button>
             </router-link>
             <router-link to="/campeonatos">
                 <button class="btn btn-light"> Campeonatos</button>
