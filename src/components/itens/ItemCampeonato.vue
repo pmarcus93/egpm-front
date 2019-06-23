b95a9cefa2e588e3502403330a9166f823316ffb
Marcus Pereira <pereiramarcus93@gmail.com>
2019-06-23 20:38:03 -0300

    Reescrevendo o HTML do componente.

--- src/components/itens/ItemCampeonato.vue
+++ src/components/itens/ItemCampeonato.vue
@@ -1,19 +1,18 @@
 <template>
-    <div class="col-6 col-lg-4 bloco-campeonato" style="border: 1px solid red">
+    <div class="col-6 col-md-4 bloco-campeonato" style="padding-left: 0; padding-right:0">
         <router-link :to="'/campeonatos/' + rota">
-            <div class="row">
-                <div class="col-12 info-campeonatos">
-                    <h3 class="nome-campeonato text-center">{{titulo}}</h3>
-                </div>
-                <div class="wrapper">
-                    <picture>
-                        <source media="(min-width: 576px)" v-bind:srcset="imagens.sm.st_url">
-                        <source media="(min-width: 768px)" v-bind:srcset="imagens.md.st_url">
-                        <source media="(min-width: 992px)" v-bind:srcset="imagens.lg.st_url">
-                        <img v-bind:srcset="imagens.default.st_url">
-                    </picture>
-                </div>
+            <div class="wrapper">
+                <h1 class="titulo-jogo">{{titulo}}</h1>
+
+                <picture>
+                    <source media="(min-width: 576px)" v-bind:srcset="imagens.sm.st_url">
+                    <source media="(min-width: 768px)" v-bind:srcset="imagens.md.st_url">
+                    <source media="(min-width: 992px)" v-bind:srcset="imagens.lg.st_url">
+                    <img class="img-fluid" v-bind:srcset="imagens.default.st_url">
+                </picture>
+
                 <div class="sobre-imagem"></div>
+
                 <div class="icones">
                     <div v-if="+iscampeonato === 1" class="d-flex align-items-center justify-content-center">
                         <span class="campeonato-txt">CAMPEONATO</span>
@@ -37,20 +36,9 @@
 
 <style scoped lang="scss">
 
-    picture {
-        border: 5px solid green;
-        display: block;
-        img {
-            width: 100%;
-            height: auto;
-        }
-    }
-
 
     .bloco-campeonato {
-        padding-bottom: 90px;
-        padding-top: 90px;
-
+        text-decoration: none;
         @include md {
             :active .sobre-imagem {
                 background: rgba(0, 0, 0, .4);
@@ -60,30 +48,10 @@
                 background: rgba(0, 0, 0, .4);
             }
         }
-
     }
-
-    .bloco-campeonato > * {
-        text-decoration: none;
-    }
-
 
     .campeonato-txt {
         color: $color-primary;
-    }
-
-    .wrapper {
-        height: 200px;
-        width: auto;
-        margin: auto;
-        overflow: hidden;
-        position: absolute;
-        top: 0;
-        z-index: 1;
-    }
-
-    .info-campeonatos {
-        z-index: 3;
     }
 
     .icone-campeonato {
@@ -101,16 +69,6 @@
         z-index: 3;
     }
 
-    .nome-campeonato {
-        color: $color-primary;
-        font-size: 1.3em;
-        font-weight: bold;
-        text-transform: uppercase;
-        @include md {
-            font-size: 2em;
-        }
-    }
-
     .sobre-imagem {
         background: rgba(0, 0, 0, .5);
         height: 100%;
@@ -118,10 +76,34 @@
         top: 0;
         transition: all linear .2s;
         width: 100%;
-        z-index: 2;
+        z-index: 1;
 
         @include md {
             background: rgba(0, 0, 0, .8);
+        }
+    }
+
+    .titulo-jogo {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 1.4rem;
+        color: $color-primary;
+        font-weight: bold;
+        text-transform: uppercase;
+        @include md {
+            font-size: 1.7rem;
+        }
+        z-index: 3;
+    }
+
+    .wrapper {
+        position: relative;
+        text-align: center;
+        img {
+            height: 300px;
+            object-fit: cover;
         }
     }
 
