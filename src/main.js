f9698ce08a9283618d08aeb4721ca2ee1dbd269e
Lucas <lucasmg37@gmail.com>
2019-06-24 08:36:22 -0300

    Removendo arquivos e rotas não utilizadas

--- src/main.js
+++ src/main.js
@@ -2,21 +2,18 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
-import jQuery from 'jquery';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
 import 'pnotify/dist/es/PNotify';
 import 'pnotify/dist/es/PNotifyButtons';
-import CKEditor from '@ckeditor/ckeditor5-vue';
+
 
 global.PNotify = require('pnotify/dist/umd/PNotify');
 global.PNotify.defaults.delay = 3000;
 global.PNotify.defaults.styling = 'bootstrap4';
-global.jQuery = jQuery;
-global.$ = jQuery;
 
 Vue.use(VueRouter);
-Vue.use(CKEditor);
 
 const router = new VueRouter({
     mode: 'history',
