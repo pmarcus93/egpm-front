89392c3fe257d8cfc900d7c9e6bc1f03c2ad9135
Marcus Pereira <pereiramarcus93@gmail.com>
2019-06-29 12:15:51 -0300

    Trazendo jogos na tela de Jogos.

--- src/components/CampeonatoTela.vue
+++ src/components/CampeonatoTela.vue
@@ -5,31 +5,20 @@
                 descricao="Confira os detalhes dos games que estarão presentes no evento. Regras dos campeonatos, categoria, trailer,
                     plataformas e muito mais!"
                 imagens="https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260">
-
         </HeaderSubScreen>
-
-        <div class="container pb-5">
-            <div class="row">
-                <ItemCampeonato v-for="(item) in campeonatos"
-                                :titulo="item.st_nome"
-                                :imagem="item.st_imagem"
-                                :rota="item.id_jogo"
-                                :iscampeonato="item.bl_campeonato"
-                ></ItemCampeonato>
-            </div>
-        </div>
+        <Campeonatos></Campeonatos>
     </section>
 
 </template>
 
 <script>
-    import ItemCampeonato from "./itens/ItemCampeonato";
     import JogoApi from "@/services/JogoApi.js";
     import HeaderSubScreen from "./itens/HeaderSubScreen";
+    import Campeonatos from "./telas/Campeonatos";
 
     export default {
         name: "CampeonatoTela",
-        components: {HeaderSubScreen, ItemCampeonato},
+        components: {Campeonatos, HeaderSubScreen},
         created() {
             var self = this;
             JogoApi.getCampeonatos(campeonatos => {
