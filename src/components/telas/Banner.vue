8f1845a346448d75fe780ca3af1e0a25590125a8
Marcus Pereira <pereiramarcus93@gmail.com>
2019-07-04 15:43:55 -0300

    Ajustando banner.

--- src/components/telas/Banner.vue
+++ src/components/telas/Banner.vue
@@ -1,17 +1,20 @@
 <template>
-    <div class="background-banner">
-        <div class="container-fluid">
-            <section class="py-5 text-center">
-                <h2 class="text-center"><span class="badge badge-pill badge-em-breve">Em breve!</span></h2>
-                <picture class="my-5">
-                    <source media="(min-width: 576px)" srcset="../../assets/images/full-logo/full-logo-md.png">
-                    <img class="d-block mx-auto my-4 logo-full" srcset="../../assets/images/full-logo/full-logo-sm.png"
-                         alt="Logo do Terceiro Encontro Gamer de Pará de Minas">
-                </picture>
-                <div class="descricao">O maior evento de games da região.<br>
-                    <p>Feito por gamers, para gamers.</p></div>
-            </section>
-        </div>
+    <div class="banner">
+        <picture class="my-5">
+            <source media="(min-width: 576px)" srcset="../../assets/images/backgrounds/banner/banner-background-sm.jpg">
+            <source media="(min-width: 768px)" srcset="../../assets/images/backgrounds/banner/banner-background-md.jpg">
+            <img class="img-background" srcset="../../assets/images/backgrounds/banner/banner-background-default.jpg"
+                 alt="Logo do Terceiro Encontro Gamer de Pará de Minas">
+        </picture>
+        <section class="content py-5 text-center">
+            <h2 class="text-center"><span class="badge badge-pill badge-em-breve">Em breve!</span></h2>
+            <picture class="my-5">
+                <source media="(min-width: 576px)" srcset="../../assets/images/full-logo/full-logo-sm.png">
+                <img class="d-block mx-auto my-4 logo-full" srcset="../../assets/images/full-logo/full-logo-sm.png"
+                     alt="Logo do Terceiro Encontro Gamer de Pará de Minas">
+            </picture>
+            <p class="content-description-banner">O maior evento de games da região.<br>Feito por gamers, para gamers.</p>
+        </section>
     </div>
 </template>
 
@@ -23,47 +26,59 @@
 
 <style lang="scss" scoped>
 
-    .background-banner {
-        background: $color-secondary-variant url("../../assets/images/banner-background.jpg") center/cover;
+    .banner {
+        height: 50vh;
+        position: relative;
+        background-color: black;
+
+        .img-background {
+            filter: blur(2px);
+            object-fit: cover;
+            width: 100%;
+            height: 100%;
+        }
     }
 
     .badge-em-breve {
         color: #000;
         background-color: $color-primary;
         text-transform: uppercase;
-        font-size: 1rem;
+        font-size: 0.9rem;
         @include sm {
-            font-size: 1.5rem;
+            font-size: 1rem;
         }
     }
 
-    .descricao {
-        color: $color-primary;
+    .content {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 100%;
+    }
+
+    .content-description-banner {
+        font-size: 1rem !important;
         text-transform: uppercase;
-        font-size: 2.7vh;
-        @include sm {
-            font-size: 1.5rem;
+        color: $color-primary;
+        @include md {
+            font-size: 1.2rem !important;
+        }
+
+        @include lg {
+            font-size: 1.3rem !important;
         }
     }
 
     .logo-full {
-        width: 300px;
+        height: 17vh;
+        width: auto;
 
         @include sm {
-            width: 400px;
+            height: 20vh;
+            width: auto;
         }
 
-        @include md {
-            width: 450px;
-        }
-
-        @include lg {
-            width: 500px;
-        }
-
-        @include xl {
-            width: 550px;
-        }
     }
 
 </style>
