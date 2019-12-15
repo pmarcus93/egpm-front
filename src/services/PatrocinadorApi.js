b9380cc55dcbb47ca621cafd733a8b6ad058f0db
Lucas <lucas.dias@unyleya.com.br>
2019-12-15 17:15:37 -0300

    retorno-realizadores - Retornando realizadores

--- src/services/PatrocinadorApi.js
+++ src/services/PatrocinadorApi.js
@@ -9,27 +9,23 @@ export default {
     },
 
     getPatrocinadores: (callback) => {
-        var url = urlpatrocinador + "/2/getAllTipo";
+        var url = urlpatrocinador + "3/getAllTipo";
+        ConfigServices.get(url, callback);
+    },
+
+    getRealizadores: (callback) => {
+        var url = urlpatrocinador + "1/getAllTipo";
         ConfigServices.get(url, callback);
     },
 
     getApoiadores: (callback) => {
-        var url = urlpatrocinador + "/1/getAllTipo";
+        var url = urlpatrocinador + "2/getAllTipo";
         ConfigServices.get(url, callback);
     },
 
     getOne: (id_patrocinador, callback) => {
         var url = urlpatrocinador + id_patrocinador;
         ConfigServices.get(url, callback);
-    },
-
-    postPatrocinador: (patrocinador, callback) => {
-        ConfigServices.post(urlpatrocinador, patrocinador, callback);
-    },
-
-    deletePatrocinador: (id, callback) => {
-        var url = urlpatrocinador + id;
-        ConfigServices.delete(url, callback);
     },
 
 }
