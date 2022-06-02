6f7ba6db9a1c9a5c27c6c4282cac3f1dac53c94c
Marcus Pereira <marcus.pereira@getrak.com.br>
2022-06-02 11:52:04 -0300

    main: adiciona segunda vídeo na home

--- src/components/content/Midia.vue
+++ src/components/content/Midia.vue
@@ -6,7 +6,7 @@
                     <Secao titulo="#EGPM na mídia"
                            descricao=""></Secao>
                 </div>
-                <div class="col-12">
+                <div class="col-lg-6">
                     <div class="d-flex justify-content-center">
                       <iframe
                           width="560"
@@ -15,6 +15,21 @@
                           srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}
                           span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style>
                           <a href=https://www.youtube.com/embed/qBsfiDiZjV4?autoplay=1><img src=https://img.youtube.com/vi/qBsfiDiZjV4/hqdefault.jpg alt='Video The Dark Knight Rises: What Went Wrong? – Wisecrack Edition'><span>▶</span></a>"
+                          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                          allowfullscreen
+                          title="Vídeo do Youtube"
+                      ></iframe>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="d-flex justify-content-center">
+                      <iframe
+                          width="560"
+                          height="315"
+                          :src="'https://www.youtube.com/embed/fEv7AFqWGiM&autoplay=1'"
+                          srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}
+                          span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style>
+                          <a href=https://www.youtube.com/embed/fEv7AFqWGiM?autoplay=1><img src=https://img.youtube.com/vi/fEv7AFqWGiM/hqdefault.jpg alt='Video The Dark Knight Rises: What Went Wrong? – Wisecrack Edition'><span>▶</span></a>"
                           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                           allowfullscreen
                           title="Vídeo do Youtube"
