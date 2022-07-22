63c06048ef971a6ae898f6d43df38c961a11f4a9
Marcus Pereira <marcus.pereira@getrak.com.br>
2022-07-22 15:01:51 -0300

    code-cleanup: adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/vue3-essential"
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "vue"
+  ],
+  "rules": {
+    "indent" : ["error", 2]
+  }
+}
