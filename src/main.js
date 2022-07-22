93f9bc889f186694c073b82b097d27cf735b3e47
Marcus Pereira <marcus.pereira@getrak.com.br>
2022-07-22 15:05:14 -0300

    indent

--- src/main.js
+++ src/main.js
@@ -1,21 +1,22 @@
 import Vue from "@vue/compat";
 import VueRouter from 'vue-router';
 import routes from './routes';
+
 const App = () => import('./App.vue');
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-    mode: 'history',
-    routes,
-    scrollBehavior() {
-        return {x: 0, y: 0};
-    }
+  mode: 'history',
+  routes,
+  scrollBehavior() {
+    return {x: 0, y: 0};
+  }
 });
 
 Vue.config.productionTip = false;
 
 new Vue({
-    router,
-    render: h => h(App),
+  router,
+  render: h => h(App),
 }).$mount('#app');
