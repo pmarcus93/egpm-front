8ab1ca6c3a64a72396e18f9fd18cb43f5b35b235
Marcus Pereira <marcus.pereira@getrak.com.br>
2022-07-22 13:51:40 -0300

    code-cleanup: indentation

--- src/components/telas/Agenda.vue
+++ src/components/telas/Agenda.vue
@@ -1,120 +1,121 @@
 <template>
-    <section>
-        <HeaderSubScreen
-                titulo="Agenda"
-                descricao="Programe-se e aproveite ao máximo o #EGPM3!"
-                :imagem="imgBanner"
-        />
+  <section>
+    <HeaderSubScreen
+        titulo="Agenda"
+        descricao="Programe-se e aproveite ao máximo o #EGPM3!"
+        :imagem="imgBanner"
+    />
 
-        <div class="container-fluid">
-            <div class="container py-5">
+    <div class="container-fluid">
+      <div class="container py-5">
 
-                <div v-if="agendamentos.length === 0" class="row pb-4">
-                    <div class="col-12 my-1 py-3 item-agenda">
-                        <div class="info-agenda-titulo text-center">
-                            A agenda será disponibilizada em breve, fique ligado!
-                        </div>
-                    </div>
-                </div>
-
-                <div v-for="(data) in datasAgenda" :key="data" class="row pb-4">
-                    <div class="col-12 py-2">
-                        <h3 class="titulo"><i class="fa fa-calendar mx-1"></i> {{formatarData(data)}}</h3>
-                    </div>
-
-                    <agenda-drop-down
-                            v-for="(item) in agendamentosPorData[data]"
-                            :key="item.id_agenda"
-                            :agenda="item"
-                            :expand="!(!item.bl_jogo && !item.st_descricao && !item.st_observacao && !item.st_local)"
-                    />
-
-                </div>
-
+        <div v-if="agendamentos.length === 0" class="row pb-4">
+          <div class="col-12 my-1 py-3 item-agenda">
+            <div class="info-agenda-titulo text-center">
+              A agenda será disponibilizada em breve, fique ligado!
             </div>
+          </div>
         </div>
-    </section>
+
+        <div v-for="(data) in datasAgenda" :key="data" class="row pb-4">
+          <div class="col-12 py-2">
+            <h3 class="titulo"><i class="fa fa-calendar mx-1"></i> {{ formatarData(data) }}</h3>
+          </div>
+
+          <agenda-drop-down
+              v-for="(item) in agendamentosPorData[data]"
+              :key="item.id_agenda"
+              :agenda="item"
+              :expand="!(!item.bl_jogo && !item.st_descricao && !item.st_observacao && !item.st_local)"
+          />
+
+        </div>
+
+      </div>
+    </div>
+  </section>
 </template>
 
 
 <script>
-    import agendaApi from "../../services/AgendaApi";
-    const HeaderSubScreen = () => import("../itens/HeaderSubScreen");
-    const AgendaDropDown = () => import("../itens/AgendaDropDown");
-    import ImgBanner from "../../assets/images/agenda-banner.jpg";
+import agendaApi from "../../services/AgendaApi";
 
-    export default {
-        name: "Agenda",
-        components: {AgendaDropDown, HeaderSubScreen},
-        data() {
-            return {
-                imgBanner: ImgBanner,
-                agendamentos: [],
-                agendamentosPorData: [],
-                datasAgenda: [],
-            }
-        },
-        methods: {
+const HeaderSubScreen = () => import("../itens/HeaderSubScreen");
+const AgendaDropDown = () => import("../itens/AgendaDropDown");
+import ImgBanner from "../../assets/images/agenda-banner.jpg";
 
-            formatarData: function (data) {
-                let ano = data.split("-")[0];
-                let mes = data.split("-")[1];
-                let dia = data.split("-")[2];
-                return dia + "/" + mes + "/" + ano;
-            },
-
-            agruparDatas: function (agendas) {
-
-                let datas = [];
-
-                agendas.forEach(item => {
-
-                    if (datas.length === 0 || datas.indexOf(item.dt_data) < 0) {
-                        datas.push(item.dt_data);
-                    }
-                });
-
-                let agendaPorData = [];
-
-                datas.forEach(data => {
-                    agendaPorData[data] = agendas.filter(agenda => {
-                        return agenda.dt_data === data;
-                    })
-                });
-
-                this.datasAgenda = datas;
-                this.agendamentosPorData = agendaPorData;
-
-            }
-        },
-        created() {
-            agendaApi.retornarAgenda(response => {
-                if (response.data.data === undefined) {
-                    this.agendamentos = [];
-                } else {
-                  console.log(response.data.data)
-                    // this.agendamentos = response.data.data;
-                    // this.agruparDatas(response.data.data);
-                }
-            })
-        }
+export default {
+  name: "Agenda",
+  components: {AgendaDropDown, HeaderSubScreen},
+  data() {
+    return {
+      imgBanner: ImgBanner,
+      agendamentos: [],
+      agendamentosPorData: [],
+      datasAgenda: [],
     }
+  },
+  methods: {
+
+    formatarData: function (data) {
+      let ano = data.split("-")[0];
+      let mes = data.split("-")[1];
+      let dia = data.split("-")[2];
+      return dia + "/" + mes + "/" + ano;
+    },
+
+    agruparDatas: function (agendas) {
+
+      let datas = [];
+
+      agendas.forEach(item => {
+
+        if (datas.length === 0 || datas.indexOf(item.dt_data) < 0) {
+          datas.push(item.dt_data);
+        }
+      });
+
+      let agendaPorData = [];
+
+      datas.forEach(data => {
+        agendaPorData[data] = agendas.filter(agenda => {
+          return agenda.dt_data === data;
+        })
+      });
+
+      this.datasAgenda = datas;
+      this.agendamentosPorData = agendaPorData;
+
+    }
+  },
+  created() {
+    agendaApi.retornarAgenda(response => {
+      if (response.data.data === undefined) {
+        this.agendamentos = [];
+      } else {
+        console.log(response.data.data)
+        // this.agendamentos = response.data.data;
+        // this.agruparDatas(response.data.data);
+      }
+    })
+  }
+}
 </script>
 
 <style scoped lang="scss">
-    .titulo {
-        color: $color-title;
-    }
+.titulo {
+  color: $color-title;
+}
 
-    .info-agenda-titulo {
-        font-size: 1.2em;
-        font-weight: 600;
-    }
+.info-agenda-titulo {
+  font-size: 1.2em;
+  font-weight: 600;
+}
 
-    .item-agenda {
-        border-radius: 5px;
-        background: $color-primary;
-    }
+.item-agenda {
+  border-radius: 5px;
+  background: $color-primary;
+}
 
 
 </style>
