3e30fdbf77b6b85b5465c9ef16edceb69db22854
= <=>
2022-11-01 23:49:29 -0300

    Ajusta indentação.

--- vite.config.js
+++ vite.config.js
@@ -4,34 +4,34 @@ import vue from '@vitejs/plugin-vue';
 const path = require("path");
 
 export default defineConfig({
-    plugins: [vue({
-        template: {
-            compilerOptions: {
-                compatConfig: {
-                    MODE: 2
-                }
-            }
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        compatConfig: {
+          MODE: 2
         }
-    })],
-    resolve: {
-        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-            vue: '@vue/compat',
-        },
+      }
+    }
+  })],
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      vue: '@vue/compat',
     },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
                     @import "@/styles/_variables.scss";
                     @import "@/styles/_mixins.scss";
                     @import "@/styles/_typography.scss";
                 `
-            }
-        }
-    },
-    build: {
-        rollupOptions: {}
+      }
     }
+  },
+  build: {
+    rollupOptions: {}
+  }
 })
